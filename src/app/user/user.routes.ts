ed75e425deaa988524eeae0feb703a129d1da6ff
Angelo Raso <araso@anura.com.ar>
2026-05-03 20:38:14 -0300

    Add linter

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -3,18 +3,14 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./user.component').then(m => m.UserComponent),
+    loadComponent: () => import('./user.component').then(m => m.UserComponent)
   },
   {
     path: 'edit',
-    loadComponent: () => import('./user.component').then(m => m.UserComponent),
+    loadComponent: () => import('./user.component').then(m => m.UserComponent)
   },
   {
     path: 'add',
-    loadComponent: () => import('./user.component').then(m => m.UserComponent),
-  },
-  {
-    path: 'advanced',
-    loadChildren: () => import('./advances.routes').then(m => m.routes),
+    loadComponent: () => import('./user.component').then(m => m.UserComponent)
   }
 ];
